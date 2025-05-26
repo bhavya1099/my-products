1773ae839099c5f67588d3b15764c1053b60a9b8
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2025-05-26 13:00:03 +0530

    Update ProductController.java

--- src/main/java/com/bootexample4/products/controller/ProductController.java
+++ src/main/java/com/bootexample4/products/controller/ProductController.java
@@ -18,6 +18,7 @@ public class ProductController {
 
     @GetMapping
     public List<Product> getAllProducts() {
+        System.out.println("new change")
         return productRepository.findAll();
     }
 
