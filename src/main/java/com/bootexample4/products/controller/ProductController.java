71fb68c84f3f3158dff16b7cddb30dc5e642cad5
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2025-06-30 16:55:03 +0530

    Update ProductController.java

--- src/main/java/com/bootexample4/products/controller/ProductController.java
+++ src/main/java/com/bootexample4/products/controller/ProductController.java
@@ -48,7 +48,7 @@ public class ProductController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteProduct(@PathVariable Long id) {
-        System.out.println("delete product");
+        System.out.println("inside delete product function");
         return productRepository.findById(id)
                 .map(product -> {
                     productRepository.delete(product);
